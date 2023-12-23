8f5a7c3410238ea9bce61b93e573cb34170c728f
FestaRexhepi <arianaarexhepi@gmail.com>
2023-12-23 22:39:54 +0100

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import 'react-toastify/dist/ReactToastify.css';
-import "./components/homepage.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import axios from "axios";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import Pages from './components/Pages';
+import Navbar from './components/Navbar';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+function App() {
+  return (
+   <>
+   <Pages/>
+   </>
+  );
+}
 
-axios.defaults.baseURL="http://localhost:5074/api";
-
-axios.interceptors.request.use((config) => {
-  const token = "Bearer " + window.localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = token;
-  }
-
-  return config;
-});
-
-root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default App;
