dd00d9d5b10a566dd7978909ddd223e05d7f3de5
FestaRexhepi <arianaarexhepi@gmail.com>
2023-12-23 19:53:48 +0100

    connecting to the mysql database

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,17 @@
+
+using Microsoft.EntityFrameworkCore;
+
+
+using backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+var connectionString = builder.Configuration.GetConnectionString("MySqlConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
 
 app.MapGet("/", () => "Hello World!");
 
