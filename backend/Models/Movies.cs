d4e36d19b17a57fd657a8b8ee58627e9ebe13a80
Ariana Rexhepi <119941160+FestaRexhepi@users.noreply.github.com>
2024-01-04 20:06:26 +0100

    model created

--- backend/Models/Movies.cs
+++ backend/Models/Movies.cs
@@ -8,5 +8,12 @@ namespace backend.Models
     public class Movies
     {
         public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public string Content { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
     }
 }
