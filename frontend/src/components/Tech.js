93ab681d0640ab438aaabd34f9c21e9a8e524ecf
Rigon Vila <121397549+RigonVila@users.noreply.github.com>
2024-01-05 13:15:24 +0100

    frontend creation

---
+++ frontend/src/components/Tech.js
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./GiftDetail";
+
+const Tech = () => {
+  const [tech, setTech] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetch = async () => {
+      const res = await axios.get("/giftideas");
+      setGifts(res.data);
+      console.log("gifts", res.data);
+    };
+    fetch();
+  }, []);
+
+  const handleMovieClick = (giftId) => {
+    navigate(`/giftideas/${giftId}`);
+  };
+
+  return (
+    <div>
+      <div className="h-container">
+        <br></br>
+        <h1>
+          <i>Favorite Gifts</i>
+          <hr></hr>
+        </h1>
+      </div>
+
+      <div className="blog-container">
+        {gifts.map((gift) => (
+          <div
+            key={gift.id}
+            className="book-card"
+            onClick={() => handleMovieClick(gift.id)}
+          >
+            <div className="blog-box">
+              <div className="image-container">
+                <img
+                  src={gift.image}
+                  alt={gift.title}
+                  className="blog-image"
+                />
+              </div>
+              <div className="text-container">
+                <h3 className="blog-title">
+                  <b>{gift.title}</b>
+                </h3>
+                <p className="blog-content">{gift.content}</p>
+              </div>
+              <div className="info-container">
+                <div className="date">{gift.date}</div>
+                <div className="author">By {gift.author}</div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div style= {{display: 'flex', justifyContent:'center', alignItems: 'center'}}>
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              1
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              2
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              3
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Tech;
---
+++ frontend/src/components/TechDetail.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const TechDetail =() => {
+    const [tech, setTech] = useState([]);
+
+    useEffect(() => {
+        const fetch = async () => {
+          const res = await axios.get("/giftideas");
+          setTech(res.data);
+          console.log("tech", res.data);
+        };
+        fetch();
+      }, []);
+
+
+  return (
+    <div>
+    {tech.map((techs) => (
+      <div key={techs.id} className="blog-detail">
+        <img src={techs.image} alt={techs.title} className="blog-image" />
+        <div className="blog-info">
+          <h2 className="blog-title">{techs.title}</h2>
+          <p className="blog-content">{techs.content}</p>
+        </div>
+        <p className="blog-description">{techs.description}</p>
+      </div>
+    ))}
+  </div>
+  );
+};
+
+export default TechDetail;
