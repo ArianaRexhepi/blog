55a82ba69d7b77ef81396906502936b0ea2bbdf8
Ariana Rexhepi <arianaarexhepi@gmail.com>
2024-01-06 19:11:21 +0100

    fixing some problems

--- frontend/src/components/GiftDetail.js
+++ frontend/src/components/GiftDetail.js
@@ -1,32 +1,37 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-const GiftDetail =() => {
-    const [gifts, setGifts] = useState([]);
+const GiftDetail = () => {
+  const [gift, setGift] = useState(null);
+  const { id } = useParams();
 
-    useEffect(() => {
-        const fetch = async () => {
-          const res = await axios.get("/giftideas");
-          setGifts(res.data);
-          console.log("gifts", res.data);
-        };
-        fetch();
-      }, []);
+  useEffect(() => {
+    const fetchGift = async () => {
+      try {
+        const res = await axios.get(`/giftideas/${id}`);
+        setGift(res.data);
+        console.log("gift", res.data);
+      } catch (error) {
+        console.error("Error fetching gift:", error);
+      }
+    };
+    fetchGift();
+  }, [id]);
 
+  if (!gift) {
+    return <p>Loading...</p>;
+  }
 
   return (
-    <div>
-    {gifts.map((gift) => (
-      <div key={gift.id} className="blog-detail">
-        <img src={gift.image} alt={gift.title} className="blog-image" />
-        <div className="blog-info">
-          <h2 className="blog-title">{gift.title}</h2>
-          <p className="blog-content">{gift.content}</p>
-        </div>
-        <p className="blog-description">{gift.description}</p>
+    <div className="blog-detail">
+      <img src={gift.image} alt={gift.title} className="blog-image" />
+      <div className="blog-info">
+        <h2 className="blog-title">{gift.title}</h2>
+        <p className="blog-content">{gift.content}</p>
       </div>
-    ))}
-  </div>
+      <p className="blog-description">{gift.description}</p>
+    </div>
   );
 };
 
