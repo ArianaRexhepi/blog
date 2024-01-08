c3b348324d28b90ad221e9c61605627b97f4e7c0
Festa Rexhepi <119941160+FestaRexhepi@users.noreply.github.com>
2024-01-08 14:16:22 +0100

    creation of  amodel

---
+++ backend/Models/Vacations.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class Vacations
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public string Description { get; set; }
+        public DateTime Year { get; set;}
+        public string Image { get; set; }
+    }
+}
